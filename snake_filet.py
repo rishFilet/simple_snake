36a0c6992cbabb93e816a77bd388f8e39eb8bdad
Rishi Khan <rishi.k@ecobee.com>
2020-05-13 18:55:04 -0400

    Finished giving directions based on keys

--- snake_filet.py
+++ snake_filet.py
@@ -1,117 +1,93 @@
-# Snake has some position DONE 
+# Snake has some position DONE
 # create a movement corresponding to the movement DONE
-# Map the keyboard input to the movement 
+# Map the keyboard input to the movement
 
 
-from pynput import keyboard
-import threading
-import time
 from turtle import Turtle, Screen
 
 
-flag = False
-
 class Gfx:
     def __init__(self):
+        super().__init__()
         self.window = Screen()
         self.window.bgcolor('black')
-        self.snake_object = Snake()
-        self.speed = self.snake_object.snake_speed
 
     def start(self):
-        self.snake_object.snake.forward(self.speed)
-        self.window.ontimer(self.start, 10)
+        self.window.mainloop()
+
 
 # Here we define the snake character/object and give it initial values for the position. direction_x and _y are used to determine which direction. +1 is up or right and -1 is down or left
-class Snake:
+class Snake(Gfx):
     def __init__(self):
         super().__init__()
-        self.x = 0
-        self.y = 0
-        self.direction_x = 0
-        self.direction_y = 0
-        self.snake = Turtle()
-        self.snake.color('green')
-        self.snake.penup()
-        self.snake_speed = 1
+        self.head = Turtle()
+        self.head.color('green')
+        self.head.penup()
+        self.snake_speed = 5
+        self.keyboard_preses()
+        self.window.listen()
+        self.movement()
 
-    #flag is a global variable used to determine when the loop should be stopped when a certain key is presed 
+    # flag is a global variable used to determine when the loop should be stopped when a certain key is presed
     def movement(self):
-        global flag
-        flag = True
-        #these if else statements start the loop from the last position the snake was in
-        if self.direction_x != 0:
-            start = self.x
-        elif self.direction_y != 0:
-            start = self.y
-        else:
-            start = 0
-        for i in range(start, 100):
-            if not flag:
-                break
-            self.x = self.x + self.direction_x
-            self.y = self.y + self.direction_y
-            print(f"X: {self.x} Y:{self.y}")
-            time.sleep(1)
+        self.head.forward(self.snake_speed)
+        self.window.ontimer(self.movement, 10)
+
+    def keyboard_preses(self):
+        self.window.onkey(lambda: self.head.setheading(90), 'Up')
+        self.window.onkey(lambda: self.head.setheading(180), 'Left')
+        self.window.onkey(lambda: self.head.setheading(0), 'Right')
+        self.window.onkey(lambda: self.head.setheading(270), 'Down')
 
 
-class UserInput:
-    def __init__(self):
-        super().__init__()
-        self.snake = Snake()
-        self.last_key_press = ""
+Snake().start()
+#     def on_press(self, key):
+#         try:
+#             print('alphanumeric key {0} pressed, not operational for game'.format(
+#                 key.char))
+#         except AttributeError:
+#             pass
+#             # print('special key {0} pressed'.format(
+#             #     key))
 
-    def on_press(self, key):
-        try:
-            print('alphanumeric key {0} pressed, not operational for game'.format(
-                key.char))
-        except AttributeError:
-            pass
-            # print('special key {0} pressed'.format(
-            #     key))
-    
-    #Threading is when the computer creates multiple processes in parallel. Here threading is necessary to continue the loop while also listening for key presses
-    def thread(self):
-        t = threading.Thread(target=self.snake.movement)
-        t.start()
+#     #Threading is when the computer creates multiple processes in parallel. Here threading is necessary to continue the loop while also listening for key presses
+#     def thread(self):
+#         t = threading.Thread(target=self.snake.movement)
+#         t.start()
 
-# move_in_direction determines which way the snake should move and checks if the last key presses were the same as before or in the opposite direction. Snake needs to turn 90 degrees and not 180 degrees
-    def move_in_direction(self, key_press, opposite_direction, dir_x, dir_y):
-        global flag
-        if self.last_key_press != key_press and self.last_key_press != opposite_direction:
-            self.snake.direction_y = dir_y
-            self.snake.direction_x = dir_x
-            self.thread()
-        else:
-            if self.last_key_press == key_press:
-                print("Already moving in that direction, can't go faster")
-            elif self.last_key_press == opposite_direction:
-                print("Cannot move in the opposite direction, turn first")
-            flag = True
+# # move_in_direction determines which way the snake should move and checks if the last key presses were the same as before or in the opposite direction. Snake needs to turn 90 degrees and not 180 degrees
+#     def move_in_direction(self, key_press, opposite_direction, dir_x, dir_y):
+#         global flag
+#         if self.last_key_press != key_press and self.last_key_press != opposite_direction:
+#             self.snake.direction_y = dir_y
+#             self.snake.direction_x = dir_x
+#             self.thread()
+#         else:
+#             if self.last_key_press == key_press:
+#                 print("Already moving in that direction, can't go faster")
+#             elif self.last_key_press == opposite_direction:
+#                 print("Cannot move in the opposite direction, turn first")
+#             flag = True
 
-    def on_release(self, key):
-        global flag
-        flag = False
-        if key == keyboard.Key.up and not flag:
-            self.move_in_direction(key, keyboard.Key.down, 0, 1)
-        elif key == keyboard.Key.down and not flag:
-            self.move_in_direction(key, keyboard.Key.up, 0, -1)
-        elif key == keyboard.Key.left and not flag:
-            self.move_in_direction(key, keyboard.Key.right, -1, 0)
-        elif key == keyboard.Key.right and not flag:
-            self.move_in_direction(key, keyboard.Key.left, 1, 0)
-        elif key == keyboard.Key.esc:
-            print("stopped")
-            print("Game Over")
-        self.last_key_press = key
+#     def on_release(self, key):
+#         global flag
+#         flag = False
+#         if key == keyboard.Key.up and not flag:
+#             self.move_in_direction(key, keyboard.Key.down, 0, 1)
+#         elif key == keyboard.Key.down and not flag:
+#             self.move_in_direction(key, keyboard.Key.up, 0, -1)
+#         elif key == keyboard.Key.left and not flag:
+#             self.move_in_direction(key, keyboard.Key.right, -1, 0)
+#         elif key == keyboard.Key.right and not flag:
+#             self.move_in_direction(key, keyboard.Key.left, 1, 0)
+#         elif key == keyboard.Key.esc:
+#             print("stopped")
+#             print("Game Over")
+#         self.last_key_press = key
 
 
-u_input = UserInput()
-with keyboard.Listener(
-        on_press=u_input.on_press,
-        on_release=u_input.on_release) as listener:
-    listener.join()
-
-
-
-
+# u_input = UserInput()
+# with keyboard.Listener(
+#         on_press=u_input.on_press,
+#         on_release=u_input.on_release) as listener:
+#     listener.join()
